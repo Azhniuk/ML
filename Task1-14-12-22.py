6e732964ec4a39748dd3720913aae29b1d791173
Sofia <azhniuk.s@gmail.com>
2022-12-14 16:03:05 +0300

    Create Task1-14-12-22.py

---
+++ Task1-14-12-22.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# y = k*x + b line.
+"""
+k = 2
+b = 2
+y = 2
+xArr = []
+yArr = []
+
+
+for x in range(0, 100):
+    xArr.append(x)
+    y = k * x + b
+    yArr.append(y)
+
+plt.plot(xArr, yArr, '-r', label='y=2x+1')
+plt.show()
+
+"""
