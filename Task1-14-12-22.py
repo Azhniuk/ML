78587bac5c8d04c22ba52ddea90edfc55ff0e1fa
Sofia <azhniuk.s@gmail.com>
2022-12-15 19:00:44 +0300

    1/2

--- Task1-14-12-22.py
+++ Task1-14-12-22.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # y = k*x + b line.
-"""
 k = 2
 b = 2
 y = 2
 xArr = []
 yArr = []
+arr = np.array([[0,1,1,3], [9, 7, 4, 0]])
 
 
 for x in range(0, 100):
@@ -16,6 +16,6 @@ for x in range(0, 100):
     yArr.append(y)
 
 plt.plot(xArr, yArr, '-r', label='y=2x+1')
-plt.show()
+print(arr[0,:])
 
-"""
+plt.show()
