adb0738fcff420f055f33ef58c5ca1851c4d61d3
Sofia <azhniuk.s@gmail.com>
2022-12-19 23:46:58 +0300

    3/3

---
+++ determinant/src/script.js
@@ -0,0 +1,12 @@
+function setMatrix() {
+    document.querySelector("#heading").innerHTML = "Set matrix"
+  }
+
+
+document.querySelector("#heading").innerHTML = "Set dimension"
+
+
+const changeSelected = (e) => {
+    const $select = document.querySelector('#dimension');
+    $select.value = '2'
+  };
