2b0ad6a16452e086511136c2941c29b0b3dd9492
Sofia <azhniuk.s@gmail.com>
2023-10-13 20:46:59 +0300

    part time 1

---
+++ Part time/circuitsitem.py
@@ -0,0 +1,41 @@
+from typing import Union
+
+class CircuitsItem:
+    def __init__(self, name: str, positive_id: int = 0, negative_id: int = 0, component_value: Union[float, int] = 0.0):
+        super(CircuitsItem, self).__init__()
+        self._name = name
+        self._positive_id = positive_id
+        self._negative_id = negative_id
+        self._component_value = component_value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str):
+        self._name = new_name
+
+    @property
+    def positive_id(self):
+        return self._positive_id
+
+    @positive_id.setter
+    def positive_id(self, new_index: int):
+        self._positive_id = new_index
+
+    @property
+    def negative_id(self):
+        return self._negative_id
+
+    @negative_id.setter
+    def negative_id(self, new_index: int):
+        self._negative_id = new_index
+
+    @property
+    def component_value(self):
+        return self._component_value
+
+    @component_value.setter
+    def component_value(self, new_value: Union[float, int]):
+        self._component_value = new_value
---
+++ Part time/task3.py
@@ -0,0 +1,2 @@
+# Task 3:
+# Task: Write your own implementation of the CircuitsItem class to pass the tests from task 2.
